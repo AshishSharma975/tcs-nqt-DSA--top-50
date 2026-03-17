47a31573033a2ca8b2e5b51291941ac377cbbb8d
AshishSharma975 <ashish8389sharma@gmail.com>
2026-03-17 14:19:52 +0530

    add questions

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const prompt = require("prompt-sync")();
 
 // console.log(checEvenorOdd(4));
 
+
+
+
+
 // // check prime number
 
 // function isPrime(n) {
@@ -32,6 +36,11 @@ const prompt = require("prompt-sync")();
 
 // console.log(isPrime(18));
 
+
+
+
+
+
 // // factorial number
 
 // function factorialNum(n) {
@@ -42,6 +51,9 @@ const prompt = require("prompt-sync")();
 //   return sum
 // }
 // console.log(factorialNum(2));
+
+
+
 
 
 // fibonaaci series
@@ -61,16 +73,103 @@ const prompt = require("prompt-sync")();
 
 
 
+
+
 // reverse a number
 
-function reverseNum(n){
-    let rev = 0;
-    while(n>0){
-        rev = (rev*10) + (n%10)
-        n= Math.floor(n/10)
-    }
-    return rev
-}
-console.log(reverseNum(1234))
+// function reverseNum(n){
+//     let rev = 0;
+//     while(n>0){
+//         rev = (rev*10) + (n%10)
+//         n= Math.floor(n/10)
+//     }
+//     return rev
+// }
+// console.log(reverseNum(1234))
 
-//
+
+
+
+//palindrome -
+
+// palindrome means = 121 =>✅
+//                    123 => ❌
+function palindrome(n) {
+  let original = n;
+  let rev = 0;
+
+  while (n > 0) {
+    rev = rev * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+
+  if (original === rev) {
+    return "palindrome";
+  } else {
+    return "not palindrome";
+  }
+}
+
+console.log(palindrome(123));
+
+
+
+
+// Armstrong Number - 
+
+// 🧠 Concept
+// 👉 Har digit ka cube karke sum karo
+
+// Example:
+// 153
+// = 1³ + 5³ + 3³
+// = 1 + 125 + 27 = 153 ✔
+
+// function Armstrong(n){
+//     let original = n;
+//     let sum = 0
+
+//     while(n!==0){
+//         let digit = (n%10)
+//         sum += digit*digit*digit;
+//         n = Math.floor(n/10)
+//     }
+//     return original === sum ?"armstrong": "not armstrong"
+// }
+// console.log(Armstrong(153))
+
+
+
+
+
+
+//  8. Sum of Digits
+// 🧠 Concept
+// 👉 Number ke sab digits ko add karna
+// Example:
+// 123 → 1+2+3 = 6
+
+// function sumDigit(n){
+//     let sum = 0;
+
+//    while(n!==0){
+//     sum += n%10;
+//     n = Math.floor(n/10)
+//    }
+//    return sum
+// }
+// console.log(sumDigit(123456))
+
+
+// 9. Largest of 3 Numbers
+// 🧠 Concept
+
+// 👉 3 numbers me sabse bada kaun?
+
+
+// function Largest(a,b,c,d){
+//  return Math.max(a,b,c,d)
+// }
+// console.log(Largest(10,45,65,86))
+
+
